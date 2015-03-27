dc60da613b3a3d179755daa6eab1df82bae1be2a
Ryan Lower <rpjlower@gmail.com>
2015-03-26 21:26:17 -0700

    Combine all notes and add intro line

--- digest.go
+++ digest.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -14,17 +15,29 @@ type digest struct {
 // DigestNoteRegex is the regex used to find notes to add to the digest
 var DigestNoteRegex = regexp.MustCompile(`COMM:(.*)(\n|$)`)
 
-func (d *digest) get(since time.Time) {
+func (d *digest) get(since time.Time) []string {
 	PRs := d.client.getPullRequestsMergedSince(since)
+
+	var notes []string
 
 	for _, PR := range PRs {
 		if PR.Body != nil {
 			if match := DigestNoteRegex.FindSubmatch([]byte(*PR.Body)); match != nil {
-				// Log match
-				log.Print(string(match[1]))
+				notes = append(notes, string(match[1]))
 			}
 		}
 	}
+
+	return notes
+}
+
+func (d *digest) send(since time.Time) {
+	messages := []string{
+		"Hey @channel, here's what we've changed in the product in the last week:",
+	}
+	messages = append(messages, d.get(since)...)
+
+	log.Print(strings.Join(messages, "\n"))
 }
 
 func main() {
@@ -40,5 +53,5 @@ func main() {
 	// Currently hardcoded to be 7 days ago
 	since := time.Now().Add(time.Hour * 24 * 7 * -1)
 
-	digest.get(since)
+	digest.send(since)
 }
